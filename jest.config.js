995c94ff8208b73797de109658849542b87555a1
Corjen Moll <corjen@thinkreversed.com>
2019-06-27 09:33:49 +0200

    chore: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  setupFilesAfterEnv: '<rootDir>/other/setupEnzyme.ts'
+  setupFilesAfterEnv: ['<rootDir>/other/setupEnzyme.ts']
 }
